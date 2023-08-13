ab35229afd5961dbbb5186dbfd9140589a837337
Sasmita Meher <77139793+sasmitameher660797@users.noreply.github.com>
2023-08-13 22:22:03 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template,jsonify
+from flask_cors import CORS,cross_origin
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
@@ -36,8 +37,31 @@ def predict_datapoint():
 
         results=round(pred[0],2)
 
-        return render_template('results.html',final_result=results)
+        return render_template('index.html',results=results)
 
+@app.route('/predictAPI',methods=['POST'])
+@cross_origin()
+def predict_api():
+    if request.method=='POST':
+        data = CustomData(
+            carat = float(request.json['carat']),
+            depth = float(request.json['depth']),
+            table = float(request.json['table']),
+            x = float(request.json['x']),
+            y = float(request.json['y']),
+            z = float(request.json['z']),
+            cut = request.json['cut'],
+            color = request.json['color'],
+            clarity = request.json['clarity']
+        )
+
+        pred_df = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(pred_df)
+
+        dct = {'price':round(pred[0],2)}
+        return jsonify(dct)
+    
 
 if __name__=="__main__":
    app.run(host='0.0.0.0',debug=True)
